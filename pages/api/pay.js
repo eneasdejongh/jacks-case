f0675ff2dce3ef3f6f70d2fabdc352deb2908d87
eneasdejongh <eneas@de-jongh.eu>
2025-05-28 18:09:15 +0200

    Update pay.js

--- pages/api/pay.js
+++ pages/api/pay.js
@@ -1,3 +1,5 @@
+// test: herbouw afdwingen
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Alleen POST toegestaan' });
